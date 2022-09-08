bdb4f2524773363a46ae34afed6b9c045ebfc16d
driica <dricagithub@gmail.com>
2022-09-08 12:22:20 -0300

    first commit

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { formatLocalDate } from "utils/format";
+import { SalePage } from "types/sale";
+import { BASE_URL } from "utils/requests";
+import Pagination from "components/Pagination";
+
+export default function DataTable() {
+  const [activePage, setActivePage] = useState(0);
+  const [page, setPage] = useState<SalePage>({
+    first: true,
+    last: true,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
+      .then((response) => {
+        setPage(response.data);
+      });
+  }, [activePage]);
+
+  const changePage = (index: number) => {
+    setActivePage(index);
+  };
+
+  return (
+    <>
+      <Pagination page={page} onPageChange={changePage} />
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {page.content?.map((sale) => (
+              <tr key={sale.id}>
+                <td>{formatLocalDate(sale.date, "dd/MM/yyyy")}</td>
+                <td>{sale.seller.name}</td>
+                <td>{sale.visited}</td>
+                <td>{sale.deals}</td>
+                <td>{sale.amount.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Chart from "react-apexcharts";
+
+import { BASE_URL } from "utils/requests";
+
+import { SaleSum } from "types/sale";
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+};
+
+export default function DonutChart() {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: []
+  });
+
+  const options = {
+    legend: {
+      show: true
+    }
+  };
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+      setChartData({ labels: myLabels, series: mySeries });
+    });
+  }, []);
+
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
+}
